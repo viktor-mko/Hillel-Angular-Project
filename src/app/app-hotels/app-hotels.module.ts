702bf07947437fbe401637405a63561481a6886d
Viktor <viktor@dsi.io>
2019-12-26 13:34:32 +0200

    Code improved

--- src/app/app-hotels/app-hotels.module.ts
+++ src/app/app-hotels/app-hotels.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
 import { ProfileComponent } from './profile/profile.component';
 import { WeatherComponent } from './weather/weather.component';
-
+import { HotelsComponent } from './hotels.component';
 
 
 @NgModule({
-  declarations: [ListComponent, ProfileComponent, WeatherComponent],
+  declarations: [ListComponent, ProfileComponent, WeatherComponent, HotelsComponent],
   imports: [
     CommonModule
   ],
-  exports: [ListComponent, ProfileComponent, WeatherComponent]
+  exports: [ListComponent, ProfileComponent, WeatherComponent, HotelsComponent]
 })
 export class AppHotelsModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {AppHotelsModule} from './app-hotels/app-hotels.module';
+import { AppHotelsModule } from './app-hotels/app-hotels.module';
 
 @NgModule({
   declarations: [
